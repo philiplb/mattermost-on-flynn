89fc00f33f13dc2ddfaaa48698ff2c28cf1fef66
Philip Lehmann-Boehm <philip@philiplb.de>
2018-08-15 15:47:41 +0200

    adding default db settings coming from the Flynn appliance

--- start.sh
+++ start.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-export MM_ServiceSettings_ListenAddress="\":$PORT\""
-
-python createConfig.py
-
 TYPE=${TYPE:-"team"}
 VERSION=${VERSION:-"5.1.1"}
 FILE=mattermost-$TYPE-$VERSION-linux-amd64.tar.gz
 URL=https://releases.mattermost.com/$VERSION/$FILE
-
 echo "Downloading $URL"
 wget $URL
 
 tar -zxf $FILE
 rm $FILE
+
+export MM_ServiceSettings_ListenAddress="\":$PORT\""
+export MM_SqlSettings_DriverName=${MM_SqlSettings_DriverName:-"\"postgres\""}
+export MM_SqlSettings_DataSource=${MM_SqlSettings_DataSource:-"\""$DATABASE_URL?sslmode=disable&connect_timeout=10"\""}
+python createConfig.py
 
 cd mattermost
 bin/mattermost --config=../config.json
